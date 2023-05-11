1ae213e730a123362c02f6aa31b18192986c1114
onlyplsson <onlyplsson@gmail.com>
2023-05-12 02:28:50 +0900

    Initial commit

---
+++ src/main/java/myproject/ourmemory/OurmemoryApplication.java
@@ -0,0 +1,13 @@
+package myproject.ourmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurmemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurmemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myproject/ourmemory/OurmemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.ourmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurmemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
